9c2c88192281163770a959fcf41339efc4a89036
Guilherme Emmanuel <guilherme1.0emmanuel@gmail.com>
2015-04-21 12:17:19 -0300

    modificacao na listView

--- Champp/app/src/main/java/com/asus/embedded/champp/MainActivity.java
+++ Champp/app/src/main/java/com/asus/embedded/champp/MainActivity.java
@@ -7,19 +7,41 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.asus.embedded.champp.controller.ChampionshipController;
+import com.asus.embedded.champp.model.Championship;
+import com.asus.embedded.champp.model.ListMyChampsAdapter;
 
 import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+
+    private ListView champListView;
+    private ListMyChampsAdapter adapter;
+    private List<Championship> champs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        champListView = (ListView) findViewById(R.id.champ_list_view);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        List<Championship> champs = ChampionshipController.getInstance().getChamps();
+        adapter = new ListMyChampsAdapter(this, champs);
+        champListView.setAdapter(adapter);
+
     }
 
     @Override
@@ -42,6 +64,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void deleteItem(View v) {
+
+        adapter.removeItem((Integer) v.getTag());
+        Toast.makeText(this,"successfully deleted championship ",Toast.LENGTH_LONG).show();
     }
 
     public void newChamp(View view) {
