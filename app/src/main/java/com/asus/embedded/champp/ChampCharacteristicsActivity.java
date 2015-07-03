9cb41c1e874adf83afc64abe2b295f487273ea9c
GG <guilherme1.0emmanuel@gmail.com>
2015-07-02 22:18:30 -0300

    removendo bugs

--- app/src/main/java/com/asus/embedded/champp/ChampCharacteristicsActivity.java
+++ app/src/main/java/com/asus/embedded/champp/ChampCharacteristicsActivity.java
@@ -156,8 +156,8 @@ public class ChampCharacteristicsActivity extends ActionBarActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         // 2. Chain together various setter methods to set the dialog characteristics
-        builder.setMessage("Tem certeza que deseja iniciar este campeonato?")
-                .setTitle("Iniciar Campeonato");
+        builder.setMessage(R.string.sure_start_champ)
+                .setTitle(R.string.init_champ);
         // 3. Add the buttons
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
